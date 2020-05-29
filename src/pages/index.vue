a52cce2268ecf99ef2cbca11c5437157b507c748
ozelotjp <contact@ozelot.app>
2020-05-29 08:57:53 +0000

    feat: 初期データの投入処理を追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,7 +7,7 @@
             Hello World
           </v-card-title>
           <v-card-text>
-            <p>Now Time: {{ now() }}</p>
+            aaaa
           </v-card-text>
         </v-card>
       </v-col>
@@ -19,22 +19,14 @@
 import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
-  middleware: 'authenticated',
-  setup() {
-    const now = () => {
-      const date = new Date()
-      const y = date.getFullYear()
-      const m = ('0' + (date.getMonth() + 1)).slice(-2)
-      const d = ('0' + date.getDate()).slice(-2)
-      const h = ('0' + date.getHours()).slice(-2)
-      const i = ('0' + date.getMinutes()).slice(-2)
-      const s = ('0' + date.getSeconds()).slice(-2)
-      return `${y}-${m}-${d} ${h}:${i}:${s}`
-    }
+  setup(_, { root: { $firebase, $router } }) {
+    const exampleRef = $firebase.firestore().collection('example')
 
-    return {
-      now
-    }
+    exampleRef.get().then((exampleQuerySnapshot) => {
+      if (exampleQuerySnapshot.empty) {
+        $router.push('/init')
+      }
+    })
   }
 })
 </script>
---
+++ src/pages/init.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup(_, { root: { $firebase, $router } }) {
+    const exampleRef = $firebase.firestore().collection('example')
+    const exampleDataList = [
+      { alphabet: 'a', code: 'alpha' },
+      { alphabet: 'b', code: 'bravo' },
+      { alphabet: 'c', code: 'charlie' },
+      { alphabet: 'd', code: 'delta' },
+      { alphabet: 'e', code: 'echo' },
+      { alphabet: 'f', code: 'foxtrot' },
+      { alphabet: 'g', code: 'golf' },
+      { alphabet: 'h', code: 'hotel' },
+      { alphabet: 'i', code: 'india' },
+      { alphabet: 'j', code: 'juliet' },
+      { alphabet: 'k', code: 'kilo' },
+      { alphabet: 'l', code: 'lima' },
+      { alphabet: 'm', code: 'mike' },
+      { alphabet: 'n', code: 'november' },
+      { alphabet: 'o', code: 'oscar' },
+      { alphabet: 'p', code: 'papa' },
+      { alphabet: 'q', code: 'quebec' },
+      { alphabet: 'r', code: 'romeo' },
+      { alphabet: 's', code: 'sierra' },
+      { alphabet: 't', code: 'tango' },
+      { alphabet: 'u', code: 'uniform' },
+      { alphabet: 'v', code: 'victor' },
+      { alphabet: 'w', code: 'whiskey' },
+      { alphabet: 'x', code: 'x-ray' },
+      { alphabet: 'y', code: 'yankee' },
+      { alphabet: 'z', code: 'zulu' }
+    ]
+
+    exampleRef.get().then((exampleQuerySnapshot) => {
+      if (exampleQuerySnapshot.empty) {
+        $router.push('/init')
+      }
+    })
+
+    exampleDataList.forEach((exampleData) => {
+      exampleRef.add(exampleData)
+    })
+
+    $router.push('/')
+  }
+})
+</script>
